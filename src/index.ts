138461e3796c7e6706f44cbbd21a06f5620f5f29
Justin Firth <jmfirth@gmail.com>
2017-11-03 22:25:51 -0400

    Add dev and build tooling; linting; testing; husky

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,100 @@
-import * as puppeteer from "puppeteer";
-import * as fs from "fs";
+import * as fs from 'fs';
+import * as puppeteer from 'puppeteer';
 
 /*
     TBD:
     jpg, png, or PDF not really sure or fully customizable?
     what level Level of customizability
 */
-
 /**
  * This function is used for taking a screenshot
- * 
+ *
  * @param url Url to navigate to for taking a screenshot
  * @param filename the name of the image to save
  * @param breakpoint Object containing the resolutions for the browser viewport
  */
-async function ScreenshotPage(url: string, filename: string, breakpoint: Breakpoint){
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
+async function screenshotPage(
+  url: string,
+  filename: string,
+  breakpoint: Breakpoint
+) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-    await page.goto(url, {waitUntil: 'load'});
+  await page.goto(url, { waitUntil: 'load' });
 
-    await page.setViewport({width: breakpoint.width, height: breakpoint.height});
+  await page.setViewport({
+    height: breakpoint.height,
+    width: breakpoint.width
+  });
 
-    //TODO: use breakpoint.name with a default
-    //TODO: make the output directory configurable
-    const filepath: string = `./output/${filename}/${filename}-${breakpoint.width}x${breakpoint.height}.jpg`;
+  // TODO: use breakpoint.name with a default
+  // TODO: make the output directory configurable
+  const filepath: string = `./output/${filename}/${filename}-${breakpoint.width}x${breakpoint.height}.jpg`;
 
-    await page.screenshot({path: filepath, fullPage: true});
+  await page.screenshot({ path: filepath, fullPage: true });
 
-    await browser.close();
+  await browser.close();
 }
 
 // TODO: Improve this...
-export function PerformScreenshotSnapshot(){
-
-    //TODO: Pull this from a configuration file that can be passed in
-    const configuration: BrowseNode[] = [{
-        url: 'https://www.mwiah.com/',
-        outputName: 'mwiah-test',
-        breakpoints: [
-            {width:1920, height: 1080, name: "BIG_SCREEN"},
-            {width:480, height:640, name: "Smaller_screen"},
-        ]
+export function performScreenshotSnapshot() {
+  // TODO: Pull this from a configuration file that can be passed in
+  const configuration: BrowseNode[] = [
+    {
+      breakpoints: [
+        { width: 1920, height: 1080, name: 'BIG_SCREEN' },
+        { width: 480, height: 640, name: 'Smaller_screen' }
+      ],
+      outputName: 'mwiah-test',
+      url: 'https://www.mwiah.com/'
     },
     {
-        url: 'https://www.cubixxsolutions.com/',
-        outputName: 'cubix-test',
-        breakpoints: [
-            {width:1920, height: 1080, name: "BIG_SCREEN"},
-            {width:1366, height: 768},
-            {width:480, height:640, name: "Smaller_screen"},
-        ]
+      breakpoints: [
+        { width: 1920, height: 1080, name: 'BIG_SCREEN' },
+        { width: 1366, height: 768 },
+        { width: 480, height: 640, name: 'Smaller_screen' }
+      ],
+      outputName: 'cubix-test',
+      url: 'https://www.cubixxsolutions.com/'
     },
     {
-        url: 'https://news.ycombinator.com/',
-        outputName: 'hackernews',
-        breakpoints: [
-            {width:1920, height: 1080, name: "BIG_SCREEN"},
-            {width:1366, height: 768},
-            {width:480, height:640, name: "Smaller_screen"},
-        ]
+      breakpoints: [
+        { width: 1920, height: 1080, name: 'BIG_SCREEN' },
+        { width: 1366, height: 768 },
+        { width: 480, height: 640, name: 'Smaller_screen' }
+      ],
+      outputName: 'hackernews',
+      url: 'https://news.ycombinator.com/'
     },
     {
-        url: 'http://dev.armsconnect.org/',
-        outputName: 'arms',
-        breakpoints: [
-            {width:1920, height: 1080, name: "BIG_SCREEN"},
-            {width:1366, height: 768},
-            {width:480, height:640, name: "Smaller_screen"},
-        ]
+      breakpoints: [
+        { width: 1920, height: 1080, name: 'BIG_SCREEN' },
+        { width: 1366, height: 768 },
+        { width: 480, height: 640, name: 'Smaller_screen' }
+      ],
+      outputName: 'arms',
+      url: 'http://dev.armsconnect.org/'
     }
-];
+  ];
 
+  // loop through every node for each breakpoint and grab a snapshot
+  configuration.forEach(node => {
+    // TODO: make a new property for describing filename vs output directory
+    if (!fs.existsSync(`./output/${node.outputName}`)) {
+      fs.mkdirSync(`./output/${node.outputName}`);
+    }
 
-    //loop through every node for each breakpoint and grab a snapshot
-    configuration.forEach((node) =>{
-
-        // TODO: make a new property for describing filename vs output directory
-        if(!fs.existsSync(`./output/${node.outputName}`)){
-            fs.mkdirSync(`./output/${node.outputName}`);
-        }
-
-        // TODO: Add default breakpoints to implement
-        if(node.breakpoints){
-            
-            node.breakpoints.forEach(element => {
-                ScreenshotPage(node.url, node.outputName, element);
-            });
-        }
-    });
+    // TODO: Add default breakpoints to implement
+    if (node.breakpoints) {
+      node.breakpoints.forEach(element => {
+        screenshotPage(node.url, node.outputName, element);
+      });
+    }
+  });
 }
 
-PerformScreenshotSnapshot();
-
-
+performScreenshotSnapshot();
 /*
     A small set of default breakpoints
     2560x1600
@@ -107,5 +108,5 @@ PerformScreenshotSnapshot();
     480x800
     480x640
     480x320
-    320x240       
+    320x240
 */
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,35 +2,35 @@
 
 //TODO: Need to likely build typeguards for these for ensure runtime compliance with the configuration
 
-declare interface BrowseNode{
-    /**
+declare interface BrowseNode {
+  /**
      * The file location the images will be outputed too
      */
-    outputName:  string;
-    /**
+  outputName: string;
+  /**
      * The url to navigate to
      */
-    url: string;
+  url: string;
 
-    /**
+  /**
      * 
      */
-    breakpoints?: Breakpoint[];
+  breakpoints?: Breakpoint[];
 }
 
-declare interface Breakpoint{
-    /**
+declare interface Breakpoint {
+  /**
      * Viewport height to use for generating a snapshot
      */
-    height: number;
-    /**
+  height: number;
+  /**
      * Viewport width to use for generating a snapshot
      */
-    width: number;
-    /**
+  width: number;
+  /**
      * Suffix used for the generated snapshot
      * 
      * TODO: If not provided we should default this to {width}x{height}
      */
-    name?: string
+  name?: string;
 }
