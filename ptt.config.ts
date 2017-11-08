c51e67e32728d55831028a1091de488d16e58059
Nathan Brooker Perry <brookerperry@gmail.com>
2017-11-08 10:35:41 -0500

    Removed unnecessary examples from the typescript config

--- ptt.config.ts
+++ ptt.config.ts
@@ -7,23 +7,6 @@ const configuration = {
     path: path.join(process.cwd(), 'snapshots', 'output')
   },
   snapshot: [
-    // {
-    //   breakpoints: [
-    //     { width: 1920, height: 1080, name: 'BIG_SCREEN' },
-    //     { width: 480, height: 640, name: 'Smaller_screen' }
-    //   ],
-    //   outputName: 'mwiah-test',
-    //   url: 'https://www.mwiah.com/'
-    // },
-    // {
-    //   breakpoints: [
-    //     { width: 1920, height: 1080, name: 'BIG_SCREEN' },
-    //     { width: 1366, height: 768 },
-    //     { width: 480, height: 640, name: 'Smaller_screen' }
-    //   ],
-    //   outputName: 'cubix-test',
-    //   url: 'https://www.cubixxsolutions.com/'
-    // },
     {
       breakpoints: [
         { width: 1920, height: 1080, name: 'BIG_SCREEN' },
@@ -33,15 +16,6 @@ const configuration = {
       outputName: 'hackernews',
       url: 'https://news.ycombinator.com/'
     }
-    // {
-    //   breakpoints: [
-    //     { width: 1920, height: 1080, name: 'BIG_SCREEN' },
-    //     { width: 1366, height: 768 },
-    //     { width: 480, height: 640, name: 'Smaller_screen' }
-    //   ],
-    //   outputName: 'arms',
-    //   url: 'http://dev.armsconnect.org/'
-    // }
   ]
 };
 
